0559101d6b09697067da4110f06bf62851148b10
Salvatore Vivolo <3092390+salva1973@users.noreply.github.com>
2022-08-26 08:50:24 +0200

    added ethers to front-end

--- hardhat-html-fund-me/README.md
+++ hardhat-html-fund-me/README.md
@@ -6,3 +6,39 @@
 ## Shortcuts
 
 - '!' in VSC (emmet) -> html scaffold
+
+## Imports in Browser's Javascript
+
+- replace:
+
+```html
+<script src="./index.js" type="text/javascript"></script>
+```
+
+with:
+
+```html
+<script src="./index.js" type="module"></script>
+```
+
+and move the onclick attributes and similar
+from html to javascript.
+
+## Contracts ABI and Address
+
+You can copy the abi from the backend:
+artifacts/contracts/FundMe.sol/FundMe.json
+and paste it in constants.js in the front-end
+then import it in the javascript file.
+
+What about the address?
+We'll use a local network, therefore we could spin-up a blockchain
+from the back-end project running:
+
+- yarn hardhat node
+
+and get the address from the console.
+Or (easier) run the node from a second terminal from the front-end project.
+
+We need to connect Metamask to our local node.
+
--- hardhat-html-fund-me/index.js
+++ hardhat-html-fund-me/index.js
@@ -1,14 +1,55 @@
+// in nodejs
+// require() or import
+
+// in front-end javascript you can't use require
+// we'll make sure to be able to use import
+// NOT POSSIBLE
+// const {ethers} = require('ethers')
+// https://docs.ethers.io/v5/getting-started/#importing
+import { ethers } from './ethers-5.6.esm.min.js'
+
+import { abi, contractAddress } from './constants.js'
+
+const connectButton = document.getElementById('connectButton')
+const fundButton = document.getElementById('fundButton')
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+console.log(ethers)
+
 async function connect() {
   if (typeof window.ethereum !== 'undefined') {
     try {
       await window.ethereum.request({ method: 'eth_requestAccounts' })
-    document.getElementById('connectButton').innerHTML = 'Connected'
+      connectButton.innerHTML = 'Connected'
     } catch (error) {
       console.log(error)
     }
-    
   } else {
-    document.getElementById('connectButton').innerHTML =
-      'Please install Metamask!'
+    connectButton.innerHTML = 'Please install Metamask!'
   }
 }
+
+// fund function
+// async function fund(ethAmount) {
+async function fund() {
+  // temporary
+  const ethAmount = '77'
+  console.log(`Funding with ${ethAmount}...`)
+  if (typeof window.ethereum !== 'undefined') {
+    // provider / connection to the blockchain
+    // signer / wallet / someone with some gas
+    // contract that we are interacting with
+    // ^ ABI & Address
+    // Web3Provider similar to JsonRpcProvider we've used before
+    // Get the http endpoint from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner() // returns connected account for that wallet
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    const transactionResponse = await contract.fund({
+      value: ethers.utils.parseEther(ethAmount),
+    })
+  }
+}
+
+// withdraw
