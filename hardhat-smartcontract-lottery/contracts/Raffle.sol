0fd0440825192d5f5008fc84ddfb46c0ceee2c9b
Salvatore Vivolo <3092390+salva1973@users.noreply.github.com>
2022-09-02 08:02:39 +0200

    working on randomness

--- hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -18,15 +18,28 @@ contract Raffle is VRFConsumerBaseV2 {
   uint256 private immutable i_entranceFee;
   address payable[] private s_players; // In Storage
   VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+  bytes32 private immutable i_gasLane;
+  uint64 private immutable i_subscriptionId;
+  uint32 private immutable i_callbackGasLimit;
+  uint16 private constant REQUEST_CONFIRMATIONS = 3;
+  uint32 private constant NUM_WORDS = 1;
 
   /* Events */
   event RaffleEnter(address indexed player);
+  event RequestedRaffleWinner(uint256 indexed requestId);
 
-  constructor(address vrfCoordinatorV2, uint256 entranceFee)
-    VRFConsumerBaseV2(vrfCoordinatorV2)
-  {
+  constructor(
+    address vrfCoordinatorV2,
+    uint256 entranceFee,
+    bytes32 gasLane,
+    uint64 subscriptionId,
+    uint32 callbackGasLimit
+  ) VRFConsumerBaseV2(vrfCoordinatorV2) {
     i_entranceFee = entranceFee;
     i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+    i_gasLane = gasLane;
+    i_subscriptionId = subscriptionId;
+    i_callbackGasLimit = callbackGasLimit;
   }
 
   function enterRaffle() public payable {
@@ -49,19 +62,26 @@ contract Raffle is VRFConsumerBaseV2 {
     // Once we get it, do something with it
     // 2 transactions process (better than 1: more robust
     // against brute force simulate calling)
+    uint256 requestId = i_vrfCoordinator.requestRandomWords(
+      i_gasLane, // gasLane
+      i_subscriptionId,
+      REQUEST_CONFIRMATIONS,
+      i_callbackGasLimit,
+      NUM_WORDS
+    );
+    emit RequestedRaffleWinner(requestId);
   }
 
   function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
     internal
     override
   {
-    i_vrfCoordinator.requestRandomWords(
-      keyHash, // gasLane
-      s_subscriptionId,
-      requestConfirmations,
-      callbackGasLimit,
-      numWords
-    );
+    // MODULO
+    // s_players size 10
+    // randomNumber 202
+    // 202 % 10 = 2 (number between 0 and 9: ok for array indexes)
+    uint256 indexOfWinner = randomWords[0] % s_players.length;
+    address payable recentWinner = s_players[indexOfWinner];
   }
 
   /* View / Pure functions */
